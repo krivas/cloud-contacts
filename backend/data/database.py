6f6c4c2cfa3417bf49916910137d0ab3443ec7a9
Kevin Rivas <krivas.reyes@gmail.com>
2023-12-17 21:26:09 -0400

    Update connection string

--- backend/data/database.py
+++ backend/data/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.orm import declarative_base
 
-URL_DATABASE='mysql+pymysql://root:helloworld@localhost:3306/cloudcontactsapp'
+URL_DATABASE='mysql+pymysql://superuser:Cloudcontacts.@cloudcontactserver.mysql.database.azure.com:3306/cloudcontactsapp'
 engine = create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
